2d291cb23503d8802960f0f18b3b625ca7c2e57f
Rachel Purganan <rpurganan.inc@gmail.com>
2022-02-17 11:00:50 -0800

    Adds repl.it configuration files and index

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>PHP Page!</title>
+</head>
+<body>
+    <h1>PHP Page!</h1>
+    <p>This is HTML</p>
+    <?php
+        echo "<p>This is PHP</p>";
+    ?>
+    <p>This is HTML</p>
+
+</body>
+</html>
